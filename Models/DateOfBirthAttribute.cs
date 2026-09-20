ee10a93da7fc251d7e3f7e3c3f74abcf0e2311b0
Adolf Mathebula <adolf.mathebula@gmail.com>
2026-09-20 23:02:54 +0200

    feat: added dateofbirth validation, person data validation

---
+++ Models/DateOfBirthAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonsAPI.Models
+{
+
+    public class DateOfBirthAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(
+            object? value,
+            ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Date of birth is required.");
+            }
+
+            if (value is DateTime dateOfBirth)
+            {
+                if (dateOfBirth.Date > DateTime.Today)
+                {
+                    return new ValidationResult(
+                        "Date of birth cannot be in the future.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,19 +1,31 @@
-﻿namespace PersonsAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonsAPI.Models;
 
 public class Person
 {
     public int PersonId { get; set; }
 
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
     public string FirstName { get; set; } = string.Empty;
 
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
     public string LastName { get; set; } = string.Empty;
 
-    public DateTime DateOfBirth { get; set; }
+    [Required]
+    [DateOfBirth]
+    public DateTime? DateOfBirth { get; set; }
 
+    [EmailAddress]
+    [StringLength(100)]
     public string? Email { get; set; }
 
+    [StringLength(20)]
     public string? Phone { get; set; }
 
+    [Required]
     public int GenderId { get; set; }
 
     // public Gender Gender { get; set; } = null!;
